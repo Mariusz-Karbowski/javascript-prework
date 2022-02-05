303735290ccd1c73c0bd71288d762a497f22af6d
Mariusz-Karbowski <m.karbowski@outlook.com>
2022-02-05 15:33:11 +0100

    Conditional logic fix

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ if (randomNumber == '1') {
   computerMove = 'kamień';
 } else if (randomNumber == '2') {
   computerMove = 'papier';
-} else if (randomNumber == '3') {
+} else {
   computerMove = 'nożyce';
 } 
 printMessage('Mój ruch: ' + computerMove);
